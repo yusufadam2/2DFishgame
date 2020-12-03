b0461bee878d95109e1a2b8cbfaccf823aaaad5c
r05485ya <yusuf.adam@student.manchester.ac.uk>
2020-12-03 22:36:34 +0000

    Bounds for fish created

--- coursework_02/main.py
+++ coursework_02/main.py
@@ -13,9 +13,36 @@ x= (ws/2) - (width/2) # calculate middle
 y= (hs/2) - (hs/2)
 mainWindow.geometry('%dx%d+%d+%d' % (width, height, x, y)) #window size
 
+# Creation of canvases
 canvasMenu= Canvas(mainWindow, width=1600, height=900)
+canvasGame= Canvas(mainWindow, width=1600, height=900, bd=0)
 
-def createMenu():
+def leftKey(event): #Function used to move the fish character left
+	direction= "left"
+	fishPos= canvasGame.coords(fish)
+	if fishPos[0]>100:
+		canvasGame.move(fish, -25, 0)
+
+def rightKey(event): #Function used to move the fish character right
+	direction= "right"
+	fishPos= canvasGame.coords(fish)
+	if fishPos[0]<1500:
+		canvasGame.move(fish, 25, 0)
+
+def upKey(event): #Function used to move the fish character up
+	direction= "up"
+	fishPos= canvasGame.coords(fish)
+	if fishPos[1]>50:
+		canvasGame.move(fish, 0, -25)
+
+def downKey(event): #Function used to move the fish character down
+	direction= "down"
+	fishPos= canvasGame.coords(fish)
+	if fishPos[1]<850:
+		canvasGame.move(fish, 0, 25)
+
+
+def createMenu(): #sets the attributes for the menu canvas
 	
 	canvasMenu.config(bg="#9CEFFE")
 	canvasMenu.pack(expand= YES, fill= BOTH)
@@ -23,69 +50,55 @@ def createMenu():
 	canvasMenu.create_image(800,450, image= imgbackground)
 	canvasMenu.pack()
 
+	#tesxt used to set the name
 	Titletext= canvasMenu.create_text(width/2 , 30 , fill="black" , font=("Times 20 italic bold", 50) , text="Menu")
 
-	btnStart= Button(mainWindow, text= "Start", bg="white", height=3, width=70, command= createGame)
-	btnStart.place(relx=0.5, rely=0.5, anchor=CENTER)
+	#Necessary buttons places on the menu screen
+	btnStart= Button(canvasMenu, text= "Start", bg="white", height=3, width=70, command= lambda:[canvasMenu.destroy(), createGame()])
+	btnStart.place(relx=0.5, rely=0.501, anchor=CENTER)
 
-	btnInst= Button(mainWindow, text= "Instructions", bg="white", height=3, width=70)
+	btnInst= Button(canvasMenu, text= "Instructions", bg="white", height=3, width=70)
 	btnInst.place(relx=0.5, rely=0.559, anchor=CENTER)
 
-	btnLdrb= Button(mainWindow, text= "Leaderboard", bg="white", height=3, width=70)
+	btnLdrb= Button(canvasMenu, text= "Leaderboard", bg="white", height=3, width=70)
 	btnLdrb.place(relx=0.5, rely=0.617, anchor=CENTER)
 
-	btnSet= Button(mainWindow, text= "Settings", bg="white", height=3, width=70)
+	btnSet= Button(canvasMenu, text= "Settings", bg="white", height=3, width=70)
 	btnSet.place(relx=0.5, rely=0.674, anchor=CENTER)
 
 	mainWindow.mainloop()
 
-def createGame():
-
-	def leftKey(event):
-		direction= "left"
-		fishPos= canvasGame.coords(fish)
-		if fishPos[0]>100:
-			canvasGame.move(fish, -20, 0)
-
-	def rightKey(event):
-		direction= "right"
-		fishPos= canvasGame.coords(fish)
-		if fishPos[0]<1500:
-			canvasGame.move(fish, 20, 0)
-
-	def upKey(event):
-		direction= "up"
-		fishPos= canvasGame.coords(fish)
-		if fishPos[1]>50:
-			canvasGame.move(fish, 0, -20)
-
-	def downKey(event):
-		direction= "down"
-		fishPos= canvasGame.coords(fish)
-		if fishPos[1]<850:
-			canvasGame.move(fish, 0, 20)
-
-	print("hello")
-	width= 1600
-	height= 900
-	canvasMenu.destroy()
-
-	canvasGame= Canvas(mainWindow, width=1600, height=900, bd=0)
+def createGame(): #Function used to add the attributes for the game canvas
+	canvasGame.config()
 	canvasGame.pack(expand= YES, fill= BOTH)
+	#Create background image
 	gamebg= PhotoImage(file= "game.png")
 	canvasGame.create_image(800,450, image= gamebg)
 
+	#create fish character image 
 	sidat= PhotoImage(file="fish.png")
-	fish=canvasGame.create_image(90,450, image=sidat)
+	global fish
+	fish=canvasGame.create_image(90,450, image= sidat)
 	canvasGame.config(bg="#9CEFFE")
 
+	#binds the control keys to the necessary functions for movement
 	canvasGame.bind("<Left>", leftKey)
 	canvasGame.bind("<Right>", rightKey)
 	canvasGame.bind("<Up>", upKey)
 	canvasGame.bind("<Down>", downKey)
 	canvasGame.focus_set()
 
-	
+	enemyList= []
+	enemyPic= []
+	colour= ["Red", "Green", "Orange", "Pink", "Purple"]
+
+	for i in range (10):
+		f_col= random.randint(0, 4)
+		y= random.randint(0,900)
+		enemyPic.append(PhotoImage(file= colour[f_col] + ".png"))
+
+		enemyList.append(canvasGame.create_image(800, y, image= enemyPic[i]))
+		print("works")
 
 	mainWindow.mainloop()
 
